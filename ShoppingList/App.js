0441659102629a7d585caa45483c010cdbe7251e
thomas <thomas.mathers@outlook.com>
2020-04-11 01:55:36 +1200

    added a delete function

--- ShoppingList/App.js
+++ ShoppingList/App.js
@@ -1,12 +1,29 @@
-import React from 'react'
-import { View, Text, Image, StyleSheet } from 'react-native'
+import React, {useState} from 'react'
+import { View, Text, FlatList, StyleSheet } from 'react-native'
+import Header from './components/Header'
+import ListItem from './components/ListItem'
+import {uuid} from 'uuidv4'
 
 const App = () => {
+  const [items, setItems] = useState([
+    {id: uuid(), text: 'Milk'},
+    {id: uuid(), text: 'Eggs'},
+    {id: uuid(), text: 'Bread'},
+    {id: uuid(), text: 'Juice'}
+
+   ])
+  const deleteItem = (id) => {
+    setItems(prevItem => {
+      return prevItem.filter(item => item.id != id)
+    });
+  }
   return (
     <View style ={styles.container}>
-      <Text style={styles.text}> What a good Shopping App</Text>
-      <Image source ={{uri: 'https://randomuser.me/api/portraits/men/1.jpg'}}
-      style={styles.img} />
+      <Header title="My Shopping List"/>
+      <FlatList 
+      data= {items} 
+      renderItem = {({item}) => <ListItem item={item} deleteItem={deleteItem} />}
+      />
     </View>
 
   )
@@ -15,17 +32,6 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1, 
-    justifyContent: 'center', 
-    alignItems: 'center'
-  },
-  text: {
-    color: 'darkslateblue',
-    fontSize:30
-  },
-  img: {
-    width: 100,
-    height:100,
-    borderRadius: 100/2
   }
 })
 export default App
